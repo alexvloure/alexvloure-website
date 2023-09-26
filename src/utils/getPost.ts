0e9409325c32fa5bfe38d6a1808e6a5a94dcf330
alex <alejandrovloure@gmail.com>
2023-09-26 17:10:45 +0200

    fix paths not working on vercel

--- src/utils/getPost.ts
+++ src/utils/getPost.ts
@@ -3,6 +3,7 @@ import { serialize } from 'next-mdx-remote/serialize';
 import fs from 'fs/promises';
 import { cache } from 'react';
 import { redirect } from 'next/navigation';
+import path from 'path';
 
 export type Frontmatter = {
   title: string;
@@ -20,12 +21,15 @@ type Post<TFrontmatter> = {
 };
 
 export const getPosts = cache(async (): Promise<Post<Frontmatter>[]> => {
-  const fileNames = await fs.readdir('src/content');
+  const workDirPath = process.cwd();
+  const contentPath = 'src/content';
+  const filePath = path.join(workDirPath, contentPath);
+  const fileNames = await fs.readdir(filePath);
 
   const posts = await Promise.all(
     fileNames.map(async (fileName) => {
       const slug = fileName.replace('.mdx', '');
-      const source = await fs.readFile(`src/content/${fileName}`, 'utf-8');
+      const source = await fs.readFile(`${filePath}/${fileName}`, 'utf-8');
       const mdxSerialized = await serialize(source, { parseFrontmatter: true });
 
       return {
