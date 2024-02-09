42823ae0772f1a61dff8db45418bf20bef3a0a74
alex <alejandrovloure@gmail.com>
2024-02-09 17:21:21 +0100

    Implement prefetch by using Link component instead of router.push

--- src/components/blog-components/BlogHeader.tsx
+++ src/components/blog-components/BlogHeader.tsx
@@ -1,24 +1,16 @@
-'use client';
-
-import { useRouter } from 'next-nprogress-bar';
-import { useEffect } from 'react';
+import Link from 'next/link';
 import { IoMdArrowBack } from 'react-icons/io';
 
 export default function BlogHeader() {
-  const router = useRouter();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <div className="w-full min-h-[100px] flex flex-col justify-between items-center gap-5 overflow-hidden relative">
-      <button
+      <Link
         className="cursor-pointer self-start items-center flex"
-        onClick={() => router.push('/', { scroll: false })}>
+        href={'/'}
+        scroll={false}>
         <IoMdArrowBack className="text-lg md:text-3xl mr-3 md:mr-5" />
         <h2 className="font-bold text-lg md:text-3xl">home</h2>
-      </button>
+      </Link>
       <div className="flex items-center">
         <h1 className="font-bold text-4xl md:text-5xl !leading-normal bg-clip-text text-transparent dark:bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500">
           Blog
--- src/components/post-components/PostHeader.tsx
+++ src/components/post-components/PostHeader.tsx
@@ -1,19 +1,16 @@
-'use client';
-
-import { useRouter } from 'next-nprogress-bar';
+import Link from 'next/link';
 import { IoMdArrowBack } from 'react-icons/io';
 
 export default function PostHeader({ title }: { title: string }) {
-  const router = useRouter();
-
   return (
     <div className="w-full min-h-[150px] flex flex-col justify-between items-center gap-5 overflow-hidden relative">
-      <button
+      <Link
         className="cursor-pointer self-start items-center flex"
-        onClick={() => router.push('/blog', { scroll: false })}>
+        href={'/blog'}
+        scroll={false}>
         <IoMdArrowBack className="text-lg md:text-3xl mr-3 md:mr-5" />
         <h2 className="font-bold text-lg md:text-3xl">blog</h2>
-      </button>
+      </Link>
       <div className="flex items-center">
         <h1 className="font-bold text-4xl md:text-5xl !leading-normal bg-clip-text text-transparent dark:bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500">
           {title}
