740e991b54be7967697ff923a92927567adbb66d
alex <alejandrovloure@gmail.com>
2024-02-12 22:01:49 +0100

    Add div for toggle language

--- src/components/home-components/LanguageCard.tsx
+++ src/components/home-components/LanguageCard.tsx
@@ -44,29 +44,32 @@ export default function LanguageCard() {
     <Card
       colSpan={1}
       withDefaultColor
-      className="flex flex-col justify-center items-center hover:scale-[103%] cursor-pointer"
-      onClick={toggleLanguage}>
-      <p className="font-semibold uppercase text-5xl md:text-8xl bg-clip-text text-transparent dark:bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500">
-        {language}
-      </p>
-      <p className="font-bold transition duration-300 ease-in-out ">
-        <span
-          className={
-            language === 'en'
-              ? 'dark:text-purple-300 text-red-400'
-              : 'dark:text-white text-zinc-700'
-          }>
-          EN
-        </span>{' '}
-        <span
-          className={
-            language === 'es'
-              ? 'dark:text-purple-300 text-red-400'
-              : 'dark:text-white text-zinc-700'
-          }>
-          ES
-        </span>
-      </p>
+      className="hover:scale-[103%] cursor-pointer">
+      <div
+        onClick={toggleLanguage}
+        className="flex flex-col justify-center items-center">
+        <p className="font-semibold uppercase text-5xl md:text-8xl bg-clip-text text-transparent dark:bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500">
+          {language}
+        </p>
+        <p className="font-bold transition duration-300 ease-in-out ">
+          <span
+            className={
+              language === 'en'
+                ? 'dark:text-purple-300 text-red-400'
+                : 'dark:text-white text-zinc-700'
+            }>
+            EN
+          </span>{' '}
+          <span
+            className={
+              language === 'es'
+                ? 'dark:text-purple-300 text-red-400'
+                : 'dark:text-white text-zinc-700'
+            }>
+            ES
+          </span>
+        </p>
+      </div>
     </Card>
   );
 }
